5ecca73687dfc925db3621eaaf229c4522ee38d4
Cecilia Lopez <cecilia.lopgo@gmail.com>
2017-03-04 16:43:45 +0100

    vampire attack, jump

--- Assets/Scripts/PlayerControl.cs
+++ Assets/Scripts/PlayerControl.cs
@@ -3,138 +3,98 @@ using System.Collections;
 
 public class PlayerControl : MonoBehaviour
 {
-	[HideInInspector]
-	public bool facingRight = true;			// For determining which way the player is currently facing.
-	[HideInInspector]
-	public bool jump = false;				// Condition for whether the player should jump.
+	public bool facingRight = true;
+	public float moveForce = 365f;			
+	public float maxSpeed = 5f;				
+	public float jumpForce = 1000f;			
+	private int health = 100;
+
+	private bool jump = false;
+	private bool m_attackPressed = false;
+	private bool isAttacking = false;
+	private bool grounded = false;			
+	private Animator anim;					
 
 
-	public float moveForce = 365f;			// Amount of force added to move the player left and right.
-	public float maxSpeed = 5f;				// The fastest the player can travel in the x axis.
-	public AudioClip[] jumpClips;			// Array of clips for when the player jumps.
-	public float jumpForce = 1000f;			// Amount of force added when the player jumps.
-	public AudioClip[] taunts;				// Array of clips for when the player taunts.
-	public float tauntProbability = 50f;	// Chance of a taunt happening.
-	public float tauntDelay = 1f;			// Delay for when the taunt should happen.
-
-
-	private int tauntIndex;					// The index of the taunts array indicating the most recent taunt.
-	private Transform groundCheck;			// A position marking where to check if the player is grounded.
-	private bool grounded = false;			// Whether or not the player is grounded.
-	private Animator anim;					// Reference to the player's animator component.
-
-
-	void Awake()
-	{
-		// Setting up references.
-		groundCheck = transform.Find("groundCheck");
+	void Awake(){
 		anim = GetComponent<Animator>();
 	}
 
 
 	void Update()
 	{
-		// The player is grounded if a linecast to the groundcheck position hits anything on the ground layer.
-		grounded = Physics2D.Linecast(transform.position, groundCheck.position, 1 << LayerMask.NameToLayer("Ground"));  
+		m_attackPressed = Input.GetButtonDown("Fire1");
 
-		// If the jump button is pressed and the player is grounded then the player should jump.
 		if(Input.GetButtonDown("Jump") && grounded)
 			jump = true;
+
+		if (m_attackPressed && !isAttacking) {
+			StartCoroutine (Attack());
+		}
 	}
 
 
-	void FixedUpdate ()
-	{
-		// Cache the horizontal input.
+	void FixedUpdate (){
 		float h = Input.GetAxis("Horizontal");
-
-		// The Speed animator parameter is set to the absolute value of the horizontal input.
 		anim.SetFloat("Speed", Mathf.Abs(h));
 
-		// If the player is changing direction (h has a different sign to velocity.x) or hasn't reached maxSpeed yet...
 		if(h * GetComponent<Rigidbody2D>().velocity.x < maxSpeed)
-			// ... add a force to the player.
 			GetComponent<Rigidbody2D>().AddForce(Vector2.right * h * moveForce);
 
-		// If the player's horizontal velocity is greater than the maxSpeed...
 		if(Mathf.Abs(GetComponent<Rigidbody2D>().velocity.x) > maxSpeed)
-			// ... set the player's velocity to the maxSpeed in the x axis.
 			GetComponent<Rigidbody2D>().velocity = new Vector2(Mathf.Sign(GetComponent<Rigidbody2D>().velocity.x) * maxSpeed, GetComponent<Rigidbody2D>().velocity.y);
 
-		// If the input is moving the player right and the player is facing left...
 		if(h > 0 && !facingRight)
-			// ... flip the player.
 			Flip();
-		// Otherwise if the input is moving the player left and the player is facing right...
 		else if(h < 0 && facingRight)
-			// ... flip the player.
 			Flip();
 
-		// If the player should jump...
-		if(jump)
-		{
-			// Set the Jump animator trigger parameter.
-			anim.SetTrigger("Jump");
-
-			// Play a random jump audio clip.
-			int i = Random.Range(0, jumpClips.Length);
-			AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
-
-			// Add a vertical force to the player.
+		if(jump){
+			anim.SetBool("jump", true);
+//			AudioSource.PlayClipAtPoint(jumpClips[i], transform.position);
 			GetComponent<Rigidbody2D>().AddForce(new Vector2(0f, jumpForce));
-
-			// Make sure the player can't jump again until the jump conditions from Update are satisfied.
 			jump = false;
 		}
 	}
 	
 	
-	void Flip ()
-	{
-		// Switch the way the player is labelled as facing.
+	void Flip () {
 		facingRight = !facingRight;
-
-		// Multiply the player's x local scale by -1.
 		Vector3 theScale = transform.localScale;
 		theScale.x *= -1;
 		transform.localScale = theScale;
 	}
 
-
-	public IEnumerator Taunt()
-	{
-		// Check the random chance of taunting.
-		float tauntChance = Random.Range(0f, 100f);
-		if(tauntChance > tauntProbability)
-		{
-			// Wait for tauntDelay number of seconds.
-			yield return new WaitForSeconds(tauntDelay);
-
-			// If there is no clip currently playing.
-			if(!GetComponent<AudioSource>().isPlaying)
-			{
-				// Choose a random, but different taunt.
-				tauntIndex = TauntRandom();
-
-				// Play the new taunt.
-				GetComponent<AudioSource>().clip = taunts[tauntIndex];
-				GetComponent<AudioSource>().Play();
-			}
+	private void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.tag == "Ground" ){ 
+			grounded = true;
+			anim.SetBool("jump", false);
 		}
 	}
 
+	private void OnTriggerStay2D (Collider2D other){
+		if (other.tag == "Enemy") {
+			//if (isAttacking)
+				//other.GetComponent<Enemy>().Hurt();
+		}
+	}
 
-	int TauntRandom()
-	{
-		// Choose a random index of the taunts array.
-		int i = Random.Range(0, taunts.Length);
+	IEnumerator Attack(){
+		isAttacking = true;
+		anim.SetBool ("attack", true);
+		anim.speed = .5f;
+		yield return new WaitForSeconds (.5f);			
+		anim.SetBool ("attack", false);
+		isAttacking = false;
+		yield return null;
+	}
 
-		// If it's the same as the previous taunt...
-		if(i == tauntIndex)
-			// ... try another random taunt.
-			return TauntRandom();
-		else
-			// Otherwise return this index.
-			return i;
+	public int Health {
+		get {
+			return health;
+		}
+		set {
+			health = value;
+		}
 	}
 }
