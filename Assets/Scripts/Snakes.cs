5d44932b7e588217930fed6ac433646c64f72410
TaT! <tatibloom@gmail.com>
2017-03-04 22:27:48 +0100

    "I'm walking here! I'm walking here!"

--- Assets/Scripts/Snakes.cs
+++ Assets/Scripts/Snakes.cs
@@ -4,13 +4,43 @@ using UnityEngine;
 
 public class Snakes : MonoBehaviour {
 
-	// Use this for initialization
+	public int powerAttack = 2;
+	private PlayerControl player;
+	private Animator anim;
+	private bool isAttacking = false;
+
 	void Start () {
-		
+		anim = GetComponent<Animator>();
+		player = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerControl>();
 	}
 	
-	// Update is called once per frame
 	void Update () {
-		
+	
 	}
+
+	void OnTriggerStay2D(Collider2D other) {
+		if ( other.tag == "Player" ) {
+			Attack ();
+		}	
+	}
+
+	void OnTriggerExit2D(Collider2D other) {
+		if ( other.tag == "Player" ) {
+			isAttacking = false;
+			anim.SetBool ("isAttacking", isAttacking);
+			anim.SetBool ("isQuiet", true);
+		}
+	}
+
+	void Attack() {
+		isAttacking = true;
+		anim.SetBool ("isAttacking", isAttacking);
+		player.Health -= powerAttack;
+	}
+
+	protected bool isInsideTheFrustrum() {
+		Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+		return screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+	}
+
 }
--- Assets/Scripts/Spawner.cs
+++ Assets/Scripts/Spawner.cs
@@ -12,10 +12,14 @@ public class Spawner : MonoBehaviour {
 
 	void Spawn () {
 		int enemyIndex = Random.Range(0, enemies.Length);
-		Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
-
-		foreach(ParticleSystem p in GetComponentsInChildren<ParticleSystem>())  {
-			p.Play();
+		if ( isInsideTheFrustrum() ) {
+			Instantiate(enemies[enemyIndex], transform.position, transform.rotation);
 		}
 	}
+
+	protected bool isInsideTheFrustrum() {
+		Vector3 screenPoint = Camera.main.WorldToViewportPoint(transform.position);
+		return screenPoint.z > 0 && screenPoint.x > 0 && screenPoint.x < 1 && screenPoint.y > 0 && screenPoint.y < 1;
+	}
+
 }
